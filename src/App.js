3b4e37488ee6a65fbd812f42892e18c5cb97efa4
unknown <jonathanmbaya13@gmail.com>
2024-06-06 15:39:27 +0200

    Fin Mobile first version

--- src/App.js
+++ src/App.js
@@ -12,10 +12,39 @@ function App() {
           className="logo-img"
         />
 
-        <SimpleBlock />
+        <SimpleBlock
+          title="Un plateau technique de pointe "
+          paragraph="Vitae volutpat commodo velit vel donec musnisl eleifend tincidunt. Scelerisque at 
+          et pellentesque arcu bibendum eleifend 
+           elementum nec rutrum consectetum. Ultrices
+          eleifend elementum nec.Ultrices eleifend elementum 
+          nec rutrum consectetur."
+        />
+
       </div>
 
       <ScrollBlock/>
+
+      <div className='blockTwo'>
+        <img
+          src={process.env.PUBLIC_URL + '/implanto.jpg'}
+          alt="Logo de l'application"
+          className="logo-img"
+        />
+
+        <SimpleBlock
+          title="Implantologie"
+          paragraph="Vitae volutpat commodo velit vel donec mus nisl 
+          eleifend tincidunt. Scelerisque at et pellentesque arcu bibendum. 
+          Ultrices eleifend elementum nec rutrum consectetur cursus eleifend. 
+          Scelerisque at et pellentesque arcu bibendum. Ultrices eleifend elementum nec. 
+          Ultrices rutrum consectetur. Vitae volutpat commodo velit vel donec mus nisl 
+          eleifend tincidunt. Scelerisque at et pellentesque arcu bibendum. 
+          Consectetur cursus eleifend. Scelerisque at et pellentesque arcu bibendum. 
+          Ultrices eleifend elementum nec.Ultrices eleifend elementum nec rutrum consectetur."
+        />
+
+      </div>
 
     </div>
   );
--- src/components/SimpleBlock/SimpleBlock.jsx
+++ src/components/SimpleBlock/SimpleBlock.jsx
@@ -2,22 +2,14 @@ import React from 'react';
 import './SimpleBlock.css';
 import { Button } from '../Buttons/Button.jsx';
 
-function SimpleBlock() {
+function SimpleBlock({title ,paragraph} ) {
     return (
         <div className='simple-block'>
             
-            <h1>Un plateau technique de pointe <br /> <hr></hr></h1>
-            
+            <h1>{title}<br /><hr></hr></h1>
 
             <p>
-                
-
-                Vitae volutpat commodo velit vel donec mus
-                nisl eleifend tincidunt. Scelerisque at 
-                et pellentesque arcu bibendum eleifend 
-                <strong> elementum nec </strong>rutrum consectetum. Ultrices
-                eleifend <strong>elementum nec </strong>.Ultrices eleifend elementum 
-                nec rutrum consectetur.
+                { paragraph }
             </p>
 
 
